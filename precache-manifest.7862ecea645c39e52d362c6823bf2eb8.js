c3950831f8d763f635eb33378b367ddfd0ddaa01
michaeldunnomm <michael.dunn@omm.agency>
2020-08-07 15:57:19 +0100

    Updates

---
+++ precache-manifest.7862ecea645c39e52d362c6823bf2eb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a28bea2a232a61a6dbbbc2a80c02f0",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "94b20a15532493109718",
+    "url": "/simple-todo-app/static/css/main.97a30233.chunk.css"
+  },
+  {
+    "revision": "14e9881dbeb5149b6550",
+    "url": "/simple-todo-app/static/js/2.0ad6588a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.0ad6588a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94b20a15532493109718",
+    "url": "/simple-todo-app/static/js/main.4c85321d.chunk.js"
+  },
+  {
+    "revision": "04e37311ff53fa85c92c",
+    "url": "/simple-todo-app/static/js/runtime-main.ee6e833e.js"
+  }
+]);
